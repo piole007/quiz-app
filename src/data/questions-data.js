a3e08ffd9cfaa01419990156e7760eba325467c5
viola <pumpure@yahoo.com>
2023-07-28 03:35:34 -0600

    data files added

---
+++ src/data/questions-data.js
@@ -0,0 +1,534 @@
+export const quizData = [
+  {
+    topic: "CSS/HTML Code",
+    questions: [
+      {
+        question: "What does CSS stand for?",
+        choices: [
+          "Cascading Style Sheets",
+          "Computer Style Sheets",
+          "Creative Style Sheets",
+          "Colorful Style Sheets",
+          "Correct Style Sheets",
+        ],
+        correctAnswer: "Cascading Style Sheets",
+      },
+      {
+        question: "What is the correct HTML tag for creating a hyperlink?",
+        choices: ["<a>", "<link>", "<href>", "<hyperlink>", "<url>"],
+        correctAnswer: "<a>",
+      },
+      {
+        question: "Which HTML element is used for the largest heading?",
+        choices: ["<h6>", "<heading>", "<h1>", "<head>", "<hlg>"],
+        correctAnswer: "<h1>",
+      },
+      {
+        question:
+          "What is the correct CSS property for changing the background color?",
+        choices: [
+          "bgcolor",
+          "color",
+          "background-color",
+          "bg-color",
+          "background",
+        ],
+        correctAnswer: "background-color",
+      },
+      {
+        question: "Which attribute is used to define inline styles?",
+        choices: ["style", "css", "font", "styles", "inline-style"],
+        correctAnswer: "style",
+      },
+      {
+        question: "What does HTML stand for?",
+        choices: [
+          "Hyperlinks and Text Markup Language",
+          "Hyper Text Markup Language",
+          "Home Tool Markup Language",
+          "Hyperlink Text Markup Language",
+          "Hyperlinking Text Markup Language",
+        ],
+        correctAnswer: "Hyper Text Markup Language",
+      },
+      {
+        question: "Which HTML tag is used to define an internal style sheet?",
+        choices: ["<css>", "<style>", "<script>", "<head>", "<stylesheet>"],
+        correctAnswer: "<style>",
+      },
+      {
+        question: "What is the correct HTML for adding a background color?",
+        choices: [
+          "<background>yellow</background>",
+          "<body style='background-color:yellow;'>",
+          "<body bg='yellow'>",
+          "<bg>yellow</bg>",
+          "<bgcolor=yellow>",
+        ],
+        correctAnswer: "<body style='background-color:yellow;'>",
+      },
+      {
+        question: "Which is the correct CSS syntax?",
+        choices: [
+          "body:color=black;",
+          "{body;color:black;}",
+          "body {color: black;}",
+          "{body:color=black;}",
+          "body:color=black;",
+        ],
+        correctAnswer: "body {color: black;}",
+      },
+      {
+        question: "Which HTML element defines the title of a document?",
+        choices: ["<meta>", "<title>", "<head>", "<header>", "<section>"],
+        correctAnswer: "<title>",
+      },
+    ],
+  },
+  {
+    topic: "Geography",
+    questions: [
+      {
+        question: "What is the capital of France?",
+        choices: ["Berlin", "London", "Madrid", "Paris", "Rome"],
+        correctAnswer: "Paris",
+      },
+      {
+        question: "Which continent is the largest by land area?",
+        choices: ["Africa", "Asia", "North America", "Europe", "South America"],
+        correctAnswer: "Asia",
+      },
+      {
+        question: "What is the deepest ocean on Earth?",
+        choices: [
+          "Atlantic Ocean",
+          "Arctic Ocean",
+          "Indian Ocean",
+          "Southern Ocean",
+          "Pacific Ocean",
+        ],
+        correctAnswer: "Pacific Ocean",
+      },
+      {
+        question: "Which country is known as the 'Land of the Rising Sun'?",
+        choices: ["China", "Japan", "Korea", "Thailand", "Vietnam"],
+        correctAnswer: "Japan",
+      },
+      {
+        question: "Which river is the longest in the world?",
+        choices: ["Nile", "Amazon", "Yangtze", "Mississippi", "Danube"],
+        correctAnswer: "Nile",
+      },
+      {
+        question: "What is the smallest country in the world?",
+        choices: [
+          "Monaco",
+          "Maldives",
+          "Vatican City",
+          "San Marino",
+          "Liechtenstein",
+        ],
+        correctAnswer: "Vatican City",
+      },
+      {
+        question:
+          "Mount Everest, the highest mountain in the world, is located in which mountain range?",
+        choices: [
+          "Rocky Mountains",
+          "Andes",
+          "Alps",
+          "Himalayas",
+          "Appalachian Mountains",
+        ],
+        correctAnswer: "Himalayas",
+      },
+      {
+        question: "Which desert is the largest hot desert in the world?",
+        choices: [
+          "Sahara Desert",
+          "Arabian Desert",
+          "Gobi Desert",
+          "Kalahari Desert",
+          "Atacama Desert",
+        ],
+        correctAnswer: "Sahara Desert",
+      },
+      {
+        question: "Which country is known as the 'Land of a Thousand Lakes'?",
+        choices: ["Sweden", "Canada", "Finland", "Norway", "Russia"],
+        correctAnswer: "Finland",
+      },
+      {
+        question:
+          "In which country would you find the famous ancient city of Machu Picchu?",
+        choices: ["Peru", "Mexico", "Brazil", "Colombia", "Ecuador"],
+        correctAnswer: "Peru",
+      },
+    ],
+  },
+  {
+    topic: "Science",
+    questions: [
+      {
+        question: "What is the chemical symbol for water?",
+        choices: ["O2", "CO2", "H2O", "HO", "H2"],
+        correctAnswer: "H2O",
+      },
+      {
+        question: "Who proposed the theory of general relativity?",
+        choices: [
+          "Isaac Newton",
+          "Albert Einstein",
+          "Galileo Galilei",
+          "Nikola Tesla",
+          "Marie Curie",
+        ],
+        correctAnswer: "Albert Einstein",
+      },
+      {
+        question: "What is the largest organ in the human body?",
+        choices: ["Liver", "Heart", "Lungs", "Skin", "Brain"],
+        correctAnswer: "Skin",
+      },
+      {
+        question: "Which gas is most abundant in Earth's atmosphere?",
+        choices: ["Oxygen", "Carbon dioxide", "Nitrogen", "Hydrogen", "Argon"],
+        correctAnswer: "Nitrogen",
+      },
+      {
+        question: "What is the chemical symbol for gold?",
+        choices: ["Au", "Ag", "Go", "Gl", "Gd"],
+        correctAnswer: "Au",
+      },
+      {
+        question: "Which planet is known as the 'Red Planet'?",
+        choices: ["Venus", "Mars", "Jupiter", "Saturn", "Neptune"],
+        correctAnswer: "Mars",
+      },
+      {
+        question: "What is the unit of electric current?",
+        choices: ["Volt", "Ampere", "Ohm", "Watt", "Joule"],
+        correctAnswer: "Ampere",
+      },
+      {
+        question: "What is the freezing point of water in Celsius?",
+        choices: ["0°C", "-10°C", "10°C", "100°C", "-100°C"],
+        correctAnswer: "0°C",
+      },
+      {
+        question: "Which element is essential for photosynthesis in plants?",
+        choices: ["Oxygen", "Carbon", "Nitrogen", "Chlorine", "Magnesium"],
+        correctAnswer: "Carbon",
+      },
+      {
+        question: "What is the chemical symbol for oxygen?",
+        choices: ["O", "O2", "O3", "Ox", "Om"],
+        correctAnswer: "O",
+      },
+    ],
+  },
+  {
+    topic: "Movies",
+    questions: [
+      {
+        question: "Who directed the movie 'Inception'?",
+        choices: [
+          "Christopher Nolan",
+          "Steven Spielberg",
+          "Quentin Tarantino",
+          "James Cameron",
+          "Martin Scorsese",
+        ],
+        correctAnswer: "Christopher Nolan",
+      },
+      {
+        question: "Which movie won the Academy Award for Best Picture in 2020?",
+        choices: [
+          "Joker",
+          "Parasite",
+          "1917",
+          "Once Upon a Time in Hollywood",
+          "The Irishman",
+        ],
+        correctAnswer: "Parasite",
+      },
+      {
+        question:
+          "Which actor played the character of Jack Dawson in the movie 'Titanic'?",
+        choices: [
+          "Johnny Depp",
+          "Leonardo DiCaprio",
+          "Brad Pitt",
+          "Tom Hanks",
+          "Will Smith",
+        ],
+        correctAnswer: "Leonardo DiCaprio",
+      },
+      {
+        question:
+          "Who is the voice behind the character Buzz Lightyear in the movie 'Toy Story'?",
+        choices: [
+          "Tom Hanks",
+          "Tim Allen",
+          "Robin Williams",
+          "Eddie Murphy",
+          "Will Smith",
+        ],
+        correctAnswer: "Tim Allen",
+      },
+      {
+        question: "Which movie features the character Tony Stark as Iron Man?",
+        choices: [
+          "The Dark Knight",
+          "Iron Man",
+          "The Avengers",
+          "Captain America: The First Avenger",
+          "Guardians of the Galaxy",
+        ],
+        correctAnswer: "Iron Man",
+      },
+      {
+        question:
+          "In the movie 'The Shawshank Redemption,' who plays the role of Andy Dufresne?",
+        choices: [
+          "Morgan Freeman",
+          "Tom Hanks",
+          "Brad Pitt",
+          "Tim Robbins",
+          "Robert De Niro",
+        ],
+        correctAnswer: "Tim Robbins",
+      },
+      {
+        question:
+          "Which movie is based on the Marvel Comics character T'Challa?",
+        choices: [
+          "Spider-Man: Homecoming",
+          "Black Panther",
+          "Doctor Strange",
+          "Ant-Man",
+          "Captain Marvel",
+        ],
+        correctAnswer: "Black Panther",
+      },
+      {
+        question:
+          "Who played the role of Hermione Granger in the 'Harry Potter' film series?",
+        choices: [
+          "Emma Stone",
+          "Emma Roberts",
+          "Emma Watson",
+          "Emma Thompson",
+          "Emma Bunton",
+        ],
+        correctAnswer: "Emma Watson",
+      },
+      {
+        question: "Which movie features the character Luke Skywalker?",
+        choices: [
+          "Star Wars: The Force Awakens",
+          "Star Wars: The Last Jedi",
+          "Star Wars: A New Hope",
+          "Star Wars: Return of the Jedi",
+          "Star Wars: The Empire Strikes Back",
+        ],
+        correctAnswer: "Star Wars: A New Hope",
+      },
+      {
+        question:
+          "Who won the Academy Award for Best Actor in a Leading Role for the movie 'The Revenant'?",
+        choices: [
+          "Leonardo DiCaprio",
+          "Johnny Depp",
+          "Joaquin Phoenix",
+          "Tom Hanks",
+          "Brad Pitt",
+        ],
+        correctAnswer: "Leonardo DiCaprio",
+      },
+    ],
+  },
+  {
+    topic: "Music",
+    questions: [
+      {
+        question: "Which band released the album 'Dark Side of the Moon'?",
+        choices: [
+          "The Rolling Stones",
+          "The Beatles",
+          "Pink Floyd",
+          "Led Zeppelin",
+          "Queen",
+        ],
+        correctAnswer: "Pink Floyd",
+      },
+      {
+        question: "Who is known as the 'Queen of Pop'?",
+        choices: [
+          "Madonna",
+          "Beyoncé",
+          "Rihanna",
+          "Taylor Swift",
+          "Ariana Grande",
+        ],
+        correctAnswer: "Madonna",
+      },
+      {
+        question: "Which famous singer is known as 'The King of Pop'?",
+        choices: [
+          "Michael Jackson",
+          "Elvis Presley",
+          "Frank Sinatra",
+          "Prince",
+          "Whitney Houston",
+        ],
+        correctAnswer: "Michael Jackson",
+      },
+      {
+        question: "Which band is known for their hit song 'Bohemian Rhapsody'?",
+        choices: [
+          "The Rolling Stones",
+          "The Beatles",
+          "Queen",
+          "Led Zeppelin",
+          "The Who",
+        ],
+        correctAnswer: "Queen",
+      },
+      {
+        question: "Who is the lead vocalist of the band 'Coldplay'?",
+        choices: [
+          "Chris Martin",
+          "Bono",
+          "Thom Yorke",
+          "Freddie Mercury",
+          "Brandon Flowers",
+        ],
+        correctAnswer: "Chris Martin",
+      },
+      {
+        question: "Which singer released the album '21'?",
+        choices: ["Adele", "Taylor Swift", "Beyoncé", "Rihanna", "Lady Gaga"],
+        correctAnswer: "Adele",
+      },
+      {
+        question: "Which artist is known for the song 'Shape of You'?",
+        choices: [
+          "Ed Sheeran",
+          "Justin Bieber",
+          "Shawn Mendes",
+          "Drake",
+          "The Weeknd",
+        ],
+        correctAnswer: "Ed Sheeran",
+      },
+      {
+        question: "Who is the lead guitarist of the band 'The Rolling Stones'?",
+        choices: [
+          "John Lennon",
+          "Paul McCartney",
+          "Keith Richards",
+          "Mick Jagger",
+          "Eric Clapton",
+        ],
+        correctAnswer: "Keith Richards",
+      },
+      {
+        question: "Which female artist sang the song 'Bad Guy'?",
+        choices: [
+          "Billie Eilish",
+          "Ariana Grande",
+          "Dua Lipa",
+          "Lorde",
+          "Selena Gomez",
+        ],
+        correctAnswer: "Billie Eilish",
+      },
+      {
+        question: "Which band sang the song 'Stairway to Heaven'?",
+        choices: [
+          "The Rolling Stones",
+          "The Beatles",
+          "Led Zeppelin",
+          "Pink Floyd",
+          "Queen",
+        ],
+        correctAnswer: "Led Zeppelin",
+      },
+    ],
+  },
+  {
+    topic: "Culture",
+    questions: [
+      {
+        question: "Which festival is known as the 'Festival of Lights'?",
+        choices: ["Diwali", "Easter", "Hanukkah", "Christmas", "Holi"],
+        correctAnswer: "Diwali",
+      },
+      {
+        question: "Who painted the Mona Lisa?",
+        choices: [
+          "Leonardo da Vinci",
+          "Vincent van Gogh",
+          "Pablo Picasso",
+          "Michelangelo",
+          "Claude Monet",
+        ],
+        correctAnswer: "Leonardo da Vinci",
+      },
+      {
+        question: "Which country is famous for the ancient pyramids?",
+        choices: ["India", "Greece", "Italy", "Egypt", "China"],
+        correctAnswer: "Egypt",
+      },
+      {
+        question: "Which language is widely spoken in Brazil?",
+        choices: ["Spanish", "Portuguese", "English", "French", "Italian"],
+        correctAnswer: "Portuguese",
+      },
+      {
+        question: "Which city is known as the 'Eternal City'?",
+        choices: ["Paris", "London", "Rome", "Venice", "Barcelona"],
+        correctAnswer: "Rome",
+      },
+      {
+        question: "Who wrote the play 'Romeo and Juliet'?",
+        choices: [
+          "William Shakespeare",
+          "Jane Austen",
+          "Charles Dickens",
+          "Leo Tolstoy",
+          "F. Scott Fitzgerald",
+        ],
+        correctAnswer: "William Shakespeare",
+      },
+      {
+        question: "Which dance originated in Argentina?",
+        choices: ["Belly Dance", "Flamenco", "Salsa", "Samba", "Tango"],
+        correctAnswer: "Tango",
+      },
+      {
+        question: "What is the traditional clothing of Japan called?",
+        choices: ["Kimono", "Sari", "Kilt", "Dirndl", "Hijab"],
+        correctAnswer: "Kimono",
+      },
+      {
+        question: "Which famous landmark is located in India?",
+        choices: [
+          "Eiffel Tower",
+          "Great Wall of China",
+          "Taj Mahal",
+          "Statue of Liberty",
+          "Big Ben",
+        ],
+        correctAnswer: "Taj Mahal",
+      },
+      {
+        question:
+          "Which dance is associated with the Maori people of New Zealand?",
+        choices: ["Hula", "Samba", "Haka", "Flamenco", "Ballet"],
+        correctAnswer: "Haka",
+      },
+    ],
+  },
+];
